1b8d1c2f72b7c9ff4d23927f281baa9abd67c0e8
Romina Maza <alejandra.maza1@gmail.com>
2022-08-30 12:43:55 -0300

    agregado de archivos de ejemplo

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react'
-
 const FormLogin = () => {
 
   const [user,setUser] = useState({nameUser:"",
                                     password:""})
 
-  const alert=(value)=>{
-    return(`<div class="alert alert-warning alert-dismissible fade ${value}" role="alert">
-    Usuario o contraseña incorrecta
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>`)
-  }
 
-  const validarUser = e=>{
+  const validarUser = e =>{
     e.preventDefault();
    
     console.log(user)
-    //realizar peticion fetch
+    //realizar peticion fetch de validacion de usuario
     return true
   }
 
@@ -24,21 +17,28 @@ const FormLogin = () => {
 
   return (
     <div className='card'>
-      <div className='card-header'>Login
+      <div className='card-header'>
+        <div className="text-center">
+          <img src="/images/logo.jpeg" className="rounded" alt="logo fundacion"/>
+        </div>
         <div className='container'>
-          { (user.nameUser=="" || user.password=="")? alert("show"): alert("hidden")}
+          {(user.nameUser=="" || user.password=="")?(<div class="alert alert-warning alert-dismissible fade show" role="alert">
+              Usuario o Contraseña incorrecta
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>):""
+          }
         </div>
       </div>
       <div className='card-body'>
       <form onSubmit={validarUser}>
         <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Nombre de usuario</label>
+          <label className="form-label">Nombre de usuario</label>
           <input type="text" className="form-control" id="nameUser" name="nameUser" aria-describedby="emailHelp" 
           value={user.nameUser} 
           onChange={(e)=>setUser({...user,nameUser:e.target.value})} />
         </div>
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Contraseña</label>
+          <label className="form-label">Contraseña</label>
           <input type="password" className="form-control" id="password" value={user.password}
           onChange={(e)=>setUser({...user,password:e.target.value})} />
         </div>
