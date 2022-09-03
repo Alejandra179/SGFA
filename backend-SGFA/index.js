dcdaf18ab1bfc46cd70cfe8ba58fb14ee82eb8aa
Alejandra179 <alejandra.maza1@gmail.com>
2022-09-03 18:26:28 -0300

    actual

--- backend-SGFA/index.js
+++ backend-SGFA/index.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
+const cors = require('cors');
 const express = require('express')
 const app = express()
-
+require('./database');
+const port = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json());
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+
+app.listen(port,() => console.log(`servidor corriendo en el puerto ${port}`));
